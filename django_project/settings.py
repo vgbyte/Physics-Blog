38585034346ffc5f4123426b4dd9220f8822304c
vgbyte <vaibhav444g@gmail.com>
2023-05-01 19:42:21 +0530

    added link in allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('PHYSICS_BLOG_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-DEBUG = False
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['localhost', 'physicsblog.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', 'physicsblog.herokuapp.com', 'physics-blog-production.up.railway.app']
 
 
 # Application definition
